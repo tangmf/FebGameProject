a4c180c791c257f3d518203ceafba1ce2f4b1cb4
Tang Ming Feng <derpsnowtmf@gmail.com>
2021-02-26 16:04:50 +0800

    Added health system
    - added Damage script
    - added HealthManager script
    - added bomb gameObject

---
+++ FebGameProject/Assets/Damage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public int damage = 40;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        HealthManager player = hitInfo.GetComponent<HealthManager>();
+        if (player != null)
+        {
+            player.TakeDamage(damage);
+        }
+
+        Destroy(gameObject);
+
+    }
+}
---
+++ FebGameProject/Assets/HealthManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    public GameObject entity;
+    public int healthPoints = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //for testing
+        /*
+        if (Input.GetButtonDown("Fire1"))
+        {
+            TakeDamage(10);
+
+        }
+        */
+    }
+
+    public void TakeDamage(int damage)
+    {
+        healthPoints -= damage;
+        if(healthPoints <= 0)
+        {
+            healthPoints = 0;
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Destroy(entity);
+        Debug.Log(entity.ToString() + " has been killed");
+    }
+
+}
