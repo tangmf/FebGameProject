1fb45d0c714a2671e478297e75e7c9f7b178df22
Tang Ming Feng <derpsnowtmf@gmail.com>
2021-02-26 15:40:57 +0800

    Added player movement and ground checking
    - added Player gameObject
    - added PlayerMovement script
    - added GroundCheck script
    - added Block gameObject

---
+++ FebGameProject/Assets/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed = 5f;
+    public int jumpForce = 10;
+    public bool isGrounded = false;
+    Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            rb2d.velocity = new Vector2(Speed, rb2d.velocity.y);
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        else if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            rb2d.velocity = new Vector2(-Speed, rb2d.velocity.y);
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else
+        {
+            rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+        }
+
+        if (Input.GetKey("w") && isGrounded)
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
+        }
+    }
+
+
+}
