8fe513bd58ad6dd403ea1b0c8c66d51996f6a09f
Tang Ming Feng <derpsnowtmf@gmail.com>
2021-02-27 11:05:55 +0800

    Added enemy detection
    - added enemy gameobject
    - added enemy animations(idle and playerDetect)
    - added time manager
    - added  EnemyAI.cs

---
+++ FebGameProject/Assets/EnemyAI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+    Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        animator.SetBool("PlayerDetect", true);
+    }
+
+    void OnTriggerExit2D(Collider2D hitInfo)
+    {
+        animator.SetBool("PlayerDetect", false);
+    }
+
+}
---
+++ FebGameProject/Assets/TimeManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+    private float nextActionTime = 0.0f;
+    public float period = 10f;
+    public int startHour = 7;
+    public Text hourText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hourText.text = startHour - 1 + ":00";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextActionTime)
+        {
+            nextActionTime += period;
+            startHour++;
+            if(startHour > 24)
+            {
+                startHour = 0;
+            }
+            else
+            {
+                hourText.text = startHour + ":00";
+            }
+            
+        }
+    }
+}
