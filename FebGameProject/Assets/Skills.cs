4402d0990e313dd600e395cd3d51a0428e4f719c
Tang Ming Feng <derpsnowtmf@gmail.com>
2021-03-04 15:40:49 +0800

    Added skill scriptable object

---
+++ FebGameProject/Assets/Skills.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Skill", menuName = "Skill")]
+public class Skills : ScriptableObject
+{
+    public new string name;
+    public string desc;
+    public Sprite skillIcon;
+    public GameObject skillBullet;
+}
