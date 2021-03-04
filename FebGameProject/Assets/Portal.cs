26cebb6c17a37232d99177d574cf40060035a183
Tang Ming Feng <derpsnowtmf@gmail.com>
2021-03-04 15:36:07 +0800

    Added portals to switch scenes

---
+++ FebGameProject/Assets/Portal.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    public bool nextScene = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        if(hitInfo.gameObject.CompareTag("Player"))
+        {
+            if (nextScene)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+            else
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+            }
+        }
+    }
+}
---
+++ FebGameProject/Assets/SkillManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillManager : MonoBehaviour
+{
+    public Image skill1;
+    public Image skill2;
+    public Image skill3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
