f07e837b31226bdd758a8b4904367e6b2a2526fc
Tang Ming Feng <derpsnowtmf@gmail.com>
2021-03-04 19:16:53 +0800

    Working on skill management

--- FebGameProject/Assets/SkillManager.cs
+++ FebGameProject/Assets/SkillManager.cs
@@ -5,18 +5,55 @@ using UnityEngine.UI;
 
 public class SkillManager : MonoBehaviour
 {
-    public Image skill1;
-    public Image skill2;
-    public Image skill3;
+    private float nextActionTime = 0.0f;
+    public float period = 0.1f;
+    public Transform attackPos;
+    public List<Skills> skillList = new List<Skills>();
+    public Skills currentSkill;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (Time.time > nextActionTime)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                if(currentSkill != null)
+                {
+                    Debug.Log("Player attacked using " + currentSkill.name);
+                    GameObject newBullet = (GameObject)Instantiate(currentSkill.skillBullet, attackPos.position, attackPos.rotation);
+                    Destroy(newBullet, 2f);
+                    nextActionTime += period;
+                }
+
+
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey("1"))
+        {
+            SwapSkill(0);
+        }
+        else if (Input.GetKey("2"))
+        {
+            SwapSkill(1);
+        }
+        else if (Input.GetKey("3"))
+        {
+            SwapSkill(2);
+        }
+    }
 
+    public void SwapSkill(int i)
+    {
+        currentSkill = skillList[i];
+    }
+
+    public void AddSkill(Skills skill)
+    {
+        skillList.Add(skill);
     }
 }
