2aba2cabde5ddca68c4a8752f458fa9dea2b6125
WYS <wooyanseun@gmail.com>
2021-03-29 16:25:35 +0800

    Completed bomb prefab and fixed portal animation

--- FebGameProject/Assets/Damage.cs
+++ FebGameProject/Assets/Damage.cs
@@ -6,18 +6,9 @@ public class Damage : MonoBehaviour
 {
     public Rigidbody2D rb;
     public int damage = 40;
+    public Animator animator;
     public bool hurtPlayer = true;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     void OnTriggerEnter2D(Collider2D hitInfo)
     {
@@ -26,8 +17,11 @@ public class Damage : MonoBehaviour
             HealthManager player = hitInfo.GetComponent<HealthManager>();
             if (player != null && hurtPlayer)
             {
+                // Play Collide animation
+                animator.Play("Bomb_Explode");
                 player.TakeDamage(damage);
-                Destroy(gameObject);
+                
+                Destroy(gameObject, animator.GetCurrentAnimatorStateInfo(0).length);
             }
         }
         else if (hitInfo.CompareTag("Enemy"))
@@ -35,8 +29,11 @@ public class Damage : MonoBehaviour
             EnemyHealth enemy = hitInfo.GetComponent<EnemyHealth>();
             if (enemy != null)
             {
+                // Play Collide animation
+                animator.Play("Bomb_Explode");
                 enemy.TakeDamage(damage);
-                Destroy(gameObject);
+                
+                Destroy(gameObject, animator.GetCurrentAnimatorStateInfo(0).length);
             }
         }
         
